f7402934d06e990749db5d1399e59e0f45c67ea8
pattarat <loo_sang@hotmail.com>
2020-02-18 15:34:31 +0700

    split footer

---
+++ footer.php
@@ -0,0 +1,101 @@
+<?php session_start();?>
+    <!-- footer -->
+    <footer>
+      <div class="footerv2-w3ls">
+        <div class="footer-w3lagile-innerr">
+          <!-- footer-top -->
+          <div class="container-fluid">
+            <div class="row  footer-v2grids w3-agileits">
+
+
+              <footer class="site-footer custom-border-top">
+                <div class="container">
+                  <div class="row">
+                    <div class="col-lg-2 col-sm-6 footer-v2grid">
+                      <h4>Support</h4>
+                      <ul>
+
+                        <li>
+                          <a href="payment.html">Payment</a>
+                        </li>
+                        <li>
+                          <a href="#">Shipping</a>
+                        </li>
+                        <li>
+                          <a href="#">Cancellation & Returns</a>
+                        </li>
+                        <li>
+                          <a href="faq.html">FAQ</a>
+                        </li>
+                      </ul>
+                    </div>
+                    <div class="col-lg-5 ml-auto mb-5 mb-lg-0">
+                      <div class="row">
+                        <div class="col-md-12">
+                          <h3 class="footer-heading mb-4">Follow Us</h3>
+                        </div>
+
+
+                        <ul class="social-iconsv2 agileinfo">
+                          <li>
+                            <a href="https://web.facebook.com/gotchaofficials/">
+                              <i class="fab fa-facebook-f"></i>
+                            </a>
+                          </li>
+                          <li>
+                            <a href="https://twitter.com/?lang=th">
+                              <i class="fab fa-twitter"></i>
+                            </a>
+                          </li>
+
+                          <li>
+                            <a href="https://www.youtube.com/">
+                              <i class="fab fa-youtube"></i>
+                            </a>
+                          </li>
+                          <li>
+                            <a href="https://www.instagram.com/gotcha_official/">
+                              <i class="fab fa-linkedin-in"></i>
+                            </a>
+                          </li>
+                          <li>
+                            <a href="https://mail.google.com/">
+                              <i class="fab fa-google-plus-g"></i>
+                            </a>
+                          </li>
+                        </ul>
+
+
+
+                      </div>
+                    </div>
+
+                    <div class="col-md-6 col-lg-3">
+                      <div class="block-5 mb-5">
+                        <h3 class="footer-heading mb-4">Address</h3>
+                        <ul class="list-unstyled">
+                          <li class="address">Rajamangala University of Technology Tawan-Ok : Chakrabongse Bhuvanarth Campus
+
+                            122 Vibhavadi Rangsit Road, Din Daeng Subdistrict, Din Daeng District, Bangkok 10400</li>
+                          <br>
+                          <li class="phone"><a href="tel://25932358">02-593-2358</a></li>
+                          <li class="email">gotchaofficial@gmail.com</li>
+                        </ul>
+                      </div>
+
+
+                    </div>
+                  </div>
+                  <br><B>ADMINISTRATOR</B>
+                  <br>YANISA,VATINEE,PATTARAT
+
+                </div>
+
+
+            </div>
+          </div>
+        </div>
+
+    </footer>
+    <!-- //footer -->
+    <!-- sign up Modal -->
