346b2f599435bd9fefaa18d63f2356d24e1fe080
pattarat <loo_sang@hotmail.com>
2020-02-22 15:22:52 +0700

    create detail_user

---
+++ detail_user.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+
+<!DOCTYPE html>
+<html lang="zxx">
+
+<?php include("h.php"); ?>
+
+<body>
+    <?php include("header.php"); ?>
+    <!-- //header -->
+
+    <body>
+
+        <div class="container">
+            <h2>USER!</h2>
+            <div class="btn-group">
+                <button type="button" class="btn btn-primary">สถานะสินค้า</button>
+
+                <button type="button" class="btn btn-primary">เปลี่ยนรหัสผ่าน</button>
+            </div>
+        </div>
+
+    </body>
